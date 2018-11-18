bf05fdb5f556bd3e3bcfec4e149ce2a030171881
Ryan Sanna <ryansann@umich.edu>
2018-11-17 18:36:18 -0700

    fix tcp server issues

--- tcp/server.go
+++ tcp/server.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
+	"sync"
+	"time"
 
 	"github.com/ryansann/hydro/storage"
 	"github.com/ryansann/hydro/storage/hash"
@@ -42,11 +45,13 @@ func Logger(l *log.Logger) OptionFunc {
 // Server is a tcp server that receives commands and interacts
 // with a storage layer
 type Server struct {
-	port  string
-	log   *log.Logger
-	store storage.Storer
-	close chan struct{}
-	conns chan net.Conn
+	port     string
+	log      *log.Logger
+	store    storage.Storer
+	close    chan struct{}
+	exited   chan struct{}
+	ln       net.Listener
+	handlers sync.WaitGroup
 }
 
 // NewServer returns a configured Server instance ready to start serving
@@ -67,16 +72,26 @@ func NewServer(opts ...OptionFunc) (*Server, error) {
 	}
 
 	return &Server{
-		port:  cfg.port,
-		log:   cfg.log,
-		store: cfg.store,
-		close: make(chan struct{}),
-		conns: make(chan net.Conn),
+		port:     cfg.port,
+		log:      cfg.log,
+		store:    cfg.store,
+		close:    make(chan struct{}),
+		exited:   make(chan struct{}),
+		handlers: sync.WaitGroup{},
 	}, nil
 }
 
 // Serve starts the server
 func (s *Server) Serve() {
+	defer func() {
+		if ok := s.wait(time.Second * 10); ok {
+			s.log.Println("connection handlers exited normally")
+		} else {
+			s.log.Println("timed out waiting for connection handlers to exit")
+		}
+		close(s.exited)
+	}()
+
 	s.log.Println("accepting connections")
 
 	ln, err := net.Listen("tcp", s.port)
@@ -84,49 +99,78 @@ func (s *Server) Serve() {
 		s.log.Fatalf("fatal: %v\n", err)
 	}
 
-	go s.acceptConns(ln)
+	s.ln = ln
 
 	for {
-		select {
-		case <-s.close:
-			s.log.Println("closing")
-
-			err := ln.Close()
-			if err != nil {
-				s.log.Printf("error closing listener: %v\n", err)
+		// ln.Accept returns an error when we close the listener, this is strange behavior
+		c, err := ln.Accept()
+		if err != nil {
+			// only log the error if it isn't due to a closed network connection
+			// not sure how else to detect this since something like net.errClosed isn't exported from net package
+			if !strings.Contains(err.Error(), "closed network connection") {
+				s.log.Printf("error accepting connection: %v\n", err)
 			}
 
-			err = s.store.Close()
-			if err != nil {
-				s.log.Printf("error closing store: %v\n", err)
-			}
+			select {
+			case <-s.close:
+				s.log.Println("closing")
 
-			return
-		case c := <-s.conns:
-			go s.handleConn(c)
+				err = s.store.Close()
+				if err != nil {
+					s.log.Printf("error closing store: %v\n", err)
+				}
+
+				return
+			default:
+			}
+		} else {
+			s.handlers.Add(1)
+			go s.handle(c)
 		}
 	}
 }
 
 // Close triggers the server to stop accepting connections and close its store
 func (s *Server) Close() error {
-	defer close(s.close)
-	s.close <- struct{}{}
+	err := s.ln.Close()
+	if err != nil {
+		s.log.Printf("error closing listener: %v\n", err)
+	}
+
+	// close the close channel to tell server to shutdown
+	close(s.close)
+	// wait for server to exit (wait for handlers to finish)
+	<-s.exited
+
 	return nil
 }
 
-func (s *Server) acceptConns(ln net.Listener) {
-	for {
-		c, err := ln.Accept()
+func (s *Server) handle(c net.Conn) {
+	defer func() {
+		s.log.Printf("closing connection from: %v\n", c.RemoteAddr())
+
+		err := c.Close()
 		if err != nil {
-			s.log.Printf("error accepting connection: %v\n", err)
-			continue
+			s.log.Printf("error closing connection: %v\n", err)
 		}
 
-		s.conns <- c
-	}
+		s.handlers.Done()
+	}()
+
+	s.log.Printf("handling connection from: %v\n", c.RemoteAddr())
 }
 
-func (s *Server) handleConn(c net.Conn) {
-	s.log.Println("handling connection")
+// wait waits for wg waiting finishes normally it returns true, otherwise it returns false.
+func (s *Server) wait(timeout time.Duration) bool {
+	c := make(chan struct{})
+	go func() {
+		defer close(c)
+		s.handlers.Wait()
+	}()
+	select {
+	case <-c:
+		return true // completed normally
+	case <-time.After(timeout):
+		return false // timed out
+	}
 }
