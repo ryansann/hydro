1424e5427c512b1a02aff9b77a6de1758e59f178
Ryan Sanna <ryansann@umich.edu>
2018-11-21 21:12:48 -0700

    add Indexer interface

--- hydro.go
+++ hydro.go
@@ -6,7 +6,7 @@ import (
 	"github.com/ryansann/hydro/pb"
 )
 
-// OffsetReader defines a behavior needed for decoding, that is reading at a specific offset
+// OffsetReader defines a behavior needed for decoding, i.e. reading bytes starting at a particular offset.
 // For example, files implement this interface via their ReadAt method.
 type OffsetReader interface {
 	// ReadAt reads from a source starting from offset into dest.
@@ -34,6 +34,13 @@ type Store interface {
 	io.Closer
 }
 
-// Indexer
+// Indexer is the interface that indexes should implement. An index interacts with a store
+// in order to perform CRUD operations. This interface provides CRUD operations through
+// the Get, Set, Delete methods. An index also needs to be able to restore any necessary
+// inmemory components after a crash, so Restore method must also be implemented by an Index.
 type Indexer interface {
+	Get(key string) (string, error)
+	Set(key, val string) error
+	Del(key string) error
+	Restore() error
 }
